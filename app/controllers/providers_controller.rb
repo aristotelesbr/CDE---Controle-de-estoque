1d0a803447946f8c27757a3014569891ac4e5e6d
Aristóteles Coutinho <Ruby@localhost.localdomain>
2015-07-04 09:51:44 -0300

    Add new & show to Providers

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,14 +1,26 @@
 class ProvidersController < ApplicationController
+  before_action :find_provider, only: [:show, :edit, :update, :destroy]
+  # before_action :provider_params, only: [:create, :update]
   def index
+    @providers = Provider.all
   end
 
   def new
-  end
-
-  def show
+    @provider = Provider.new
   end
 
   def create
+    @provider = Provider.new(provider_params)
+    if @provider.save
+      redirect_to @provider,
+        notice: 'Fornecedor cadastrado com sucesso!'
+    else
+      render 'new',
+        alert: 'Não foi possivel salvar o registro.'
+    end
+  end
+
+  def show
   end
 
   def edit
@@ -22,7 +34,11 @@ class ProvidersController < ApplicationController
 
   private
 
-  def providers_params
+  def find_provider
+    @provider = Provider.find(params[:id])
+  end
+
+  def provider_params
     params.require(:provider).permit(:name, :cnpj)
   end
 end
