342d3bc4d6ce3cbc9be918156bb7998109638ffd
Aristóteles Coutinho <Ruby@localhost.localdomain>
2015-07-13 09:55:31 -0300

    Add User to Sale

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713123316) do
+ActiveRecord::Schema.define(version: 20150713125130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150713123316) do
     t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "sales", ["product_id"], name: "index_sales_on_product_id", using: :btree
